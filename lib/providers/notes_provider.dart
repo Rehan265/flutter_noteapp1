8c27faed5f174306f465b1dc3e044d2e7461b331
Rehan265 <109652975+Rehan265@users.noreply.github.com>
2024-09-27 18:53:51 +0500

    add some more code

--- lib/providers/notes_provider.dart
+++ lib/providers/notes_provider.dart
@@ -1,20 +1,43 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class NotesProvider extends ChangeNotifier {
   List<Map> notes = [];
 
+  Future<void> addDataFromLocalStorage() async {
+    final SharedPreferences sp = await SharedPreferences.getInstance();
+    sp.setString("Notes", json.encode(notes));
+  }
+
+  Future<List> getDataFromLocalStorage() async {
+    final SharedPreferences sp = await SharedPreferences.getInstance();
+    final data = sp.getString("Notes");
+
+    if (data != null) {
+      notes = List<Map>.from(json.decode(data));
+      return notes;
+    } else {
+      return [];
+    }
+  }
+
   void addNote(Map item) {
     notes.add(item);
+    addDataFromLocalStorage();
     notifyListeners();
   }
 
   void removeNote(Map item) {
     notes.remove(item);
+    addDataFromLocalStorage();
     notifyListeners();
   }
 
   void updateNote(int index, Map item) {
     notes[index] = item;
+    addDataFromLocalStorage();
     notifyListeners();
   }
 }
