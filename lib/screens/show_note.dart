2413eebfee5f7ac8ff998ac4b15533dbda641046
Rehan265 <109652975+Rehan265@users.noreply.github.com>
2024-09-27 18:52:47 +0500

    Add files via upload

--- lib/screens/show_note.dart
+++ lib/screens/show_note.dart
@@ -25,82 +25,104 @@ class ShowNote extends StatelessWidget {
         title: const Text("Note app"),
         centerTitle: true,
       ),
-      body: ListView.builder(
-        scrollDirection: Axis.vertical,
-        itemCount: notes.length,
-        itemBuilder: (context, index) {
-          Map note = notes[index];
-          colorIndex >= colors.length - 1 ? colorIndex = 0 : colorIndex++;
-          return GestureDetector(
-            onTap: () {
-              Navigator.pop(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => UpdateNote(
-                    note: note,
-                    index: index,
-                  ),
-                ),
-              );
-            },
-            onHorizontalDragUpdate: (details) {
-              showDialog(
-                context: context,
-                builder: (context) => AlertDialog(
-                  title: const Text("Delete"),
-                  content:
-                      const Text("Are you sure you want to deklete this ?"),
-                  actions: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: const Text(
-                        "No",
-                      ),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        Provider.of<NotesProvider>(context, listen: false)
-                            .removeNote(note);
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            content: Text(
-                              "Note Deleted",
-                            ),
+      body: Expanded(
+        child: FutureBuilder(
+          future: Provider.of<NotesProvider>(context).getDataFromLocalStorage(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator.adaptive();
+            } else if (snapshot.connectionState == ConnectionState.done &&
+                snapshot.hasData) {
+              final notes = snapshot.data;
+              return ListView.builder(
+                scrollDirection: Axis.vertical,
+                itemCount: notes!.length,
+                itemBuilder: (context, index) {
+                  Map note = notes[index];
+                  colorIndex >= colors.length - 1
+                      ? colorIndex = 0
+                      : colorIndex++;
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.pop(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => UpdateNote(
+                            note: note,
+                            index: index,
                           ),
-                        );
-                        Navigator.pop(context);
-                      },
-                      child: const Text(
-                        "Yes",
+                        ),
+                      );
+                    },
+                    onHorizontalDragUpdate: (details) {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: const Text("Delete"),
+                          content: const Text(
+                              "Are you sure you want to delete this ?"),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: const Text(
+                                "No",
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Provider.of<NotesProvider>(context,
+                                        listen: false)
+                                    .removeNote(note);
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    content: Text(
+                                      "Note Deleted",
+                                    ),
+                                  ),
+                                );
+                                Navigator.pop(context);
+                              },
+                              child: const Text(
+                                "Yes",
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(4),
+                      margin: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                          color: colors[colorIndex],
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Column(
+                        children: [
+                          Text(
+                            note["title"],
+                            style: const TextStyle(
+                                fontSize: 20, color: Colors.black),
+                          ),
+                          Text(
+                            note["content"].length >= 100
+                                ? "${note["content"].toString().substring(0, 100)}..."
+                                : note["content"],
+                            style: const TextStyle(
+                                fontSize: 18, color: Colors.black),
+                          )
+                        ],
                       ),
                     ),
-                  ],
-                ),
+                  );
+                },
               );
-            },
-            child: Container(
-              padding: const EdgeInsets.all(4),
-              margin: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                  color: colors[colorIndex],
-                  borderRadius: BorderRadius.circular(10)),
-              child: Column(
-                children: [
-                  Text(
-                    note["title"],
-                    style: const TextStyle(fontSize: 20, color: Colors.black),
-                  ),
-                  Text(
-                    note["content"],
-                    style: const TextStyle(fontSize: 18, color: Colors.black),
-                  )
-                ],
-              ),
-            ),
-          );
-        },
+            } else {
+              return const Center(child: Text("Some Error has Occured"));
+            }
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
